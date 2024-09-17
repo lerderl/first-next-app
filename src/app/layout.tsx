06e6b9fe62faf277bd0a1267e35bf52924edbbab
lerderl <josepholukunle1107@gmail.com>
2024-09-17 14:15:36 +0100

    feat: add navbar to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,6 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <Link href="/">Home</Link>
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
         {children}
       </body>
     </html>
